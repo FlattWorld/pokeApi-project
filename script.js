34d76b4095edc473bf50150a7ea623a0a094c266
gabo140595 <gabrielcarmona33@gmail.com>
2022-10-20 11:51:25 -0500

    fead:proyecto api

--- script.js
+++ script.js
@@ -1,1 +1,83 @@
-console.log("Hola")
+
+
+const head = document.createElement("header");
+head.id="head"
+head.innerHTML = "";
+root.appendChild(head);
+//se crea un header dentro del documento y dentro de un body
+// con id head  
+
+const img = document.createElement("img")
+img.id="img"
+img.src="./logo.png"
+head.appendChild(img);
+//se crea una imagen con id img que esta dentro de nuestro header 
+//lo llamamos con su id heaad
+// const div = document.createElement("div")
+// div.id="root"
+// document.body.appendChild(div)4
+
+
+
+const pokemonContainer = document.querySelector("#divp")
+function fetchPokemon(id)
+{
+fetch('https://pokeapi.co/api/v2/pokemon/${id}/')
+.then((res)=> res.json())
+.then((data) => createPokemon(data));
+}
+function fetchPokemons(number)
+{
+    for(const i = 1 ; i <= number;i++)
+    fetchPokemon(i)
+}
+
+function createPokemon (pokemon)
+{
+
+
+
+            const div = document.createElement("div")
+            div.id="div1"
+            root.appendChild(div)
+
+
+            const div1 = document.createElement("div")
+            div1.id="divp"
+            div.appendChild(div1)
+
+                
+            const btn = document.createElement("input")
+            btn.type="button"
+            btn.value="DETALLES"
+            btn.id="btn"
+            div1.appendChild(btn)
+
+            const imgp = document.createElement("img")
+            imgp.src = pokemon.sprites.front_defaul            
+            div1.appendChild(imgp)
+
+            const number =document-createElement("p")
+            number.textContent = '#${pokemon.id.toString(.padStart(3,0))}';
+
+            const name = document.createElement("p")
+            name.id="name"
+            name.textContent= pokemon.name
+
+            div1.appendChild(imgp)
+            div1.appendChild(name)
+            div1.appendChild(name)
+
+            pokemonContainer.appendChild(div1)
+
+            
+}
+fetchPokemons(13);
+
+
+
+
+
+
+
+
